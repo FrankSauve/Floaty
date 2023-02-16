63991558d26002d257125411e21a40d4894bc933
François Crispo-Sauvé <fcrispo@stingray.com>
2023-02-16 11:52:51 -0500

    Version 1

---
+++ Floaty/Floaty.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Floaty
+{
+    [BepInPlugin("farmk.Floaty", "Floaty", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class Floaty : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("farmk.Floaty");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(ItemDrop), "Awake")]
+        class Float_Patch
+        {
+            static void Prefix(ItemDrop __instance)
+            {
+                if (__instance.gameObject.GetComponent<Floating>() == null)
+                {
+                    Debug.Log($"Floaty: Adding floating component to: {__instance}");
+                    Floating floating = __instance.gameObject.AddComponent<Floating>();
+                    floating.m_waterLevelOffset = 0.5f;
+                }
+            }
+        }
+    }
+}
